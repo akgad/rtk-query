9c16789028b437e8426a3ea7800c735ff115b8ba
akgad <akgad004@gmai.com>
2023-05-01 22:29:49 +0300

    Finished a project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+import { useGetGoodsQuery,useAddProductsMutation,useDeleteProductsMutation } from './redux/goodsApi'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState('')
+    const [text, setText] = useState('')
+    const {data = [], isLoading} = useGetGoodsQuery(count)
+    const [addProduct, {isError}] = useAddProductsMutation()
+    const [deleteProduct] = useDeleteProductsMutation()
+
+    const handleBtnAddProduct = async () => {
+        if(text) {
+            await addProduct({name: text}).unwrap()
+        }
+        setText('')
+    }
+
+    const handleBtnDelteProduct = async (id) => {
+            await deleteProduct(id).unwrap()
+    }
+
+    if(isLoading) return <h1>Loading....</h1>
+    return (
+        <div className="App">
+            <h1>Base use RTK Query</h1>
+            <h3>CRUD on json server without PUT request</h3>
+            <div>
+                <input type='text' value={text} onChange={e => setText(e.target.value)}/>
+                <button onClick={handleBtnAddProduct}>Add product</button>
+            </div>
+
+            <div>
+                <select value={count} onChange={e => setCount(e.target.value)}>
+                    <option value=''>All</option>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                </select>
+            </div>
+            <ul>
+                {data.map(({id,name}) => (
+                    <li key={id} className='item'>
+                        {name}
+                        <span className='close' onClick={() => handleBtnDelteProduct(id)}>&times;</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
